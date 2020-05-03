839f9a01100f3b9653800c14e4d7be1ba11576e0
MarkIT2020 <64119895+MarkIT2020@users.noreply.github.com>
2020-05-03 09:15:09 +0800

    exercise7

---
+++ Exercise 7/lightbox.js
@@ -0,0 +1,32 @@
+//change the visibility of a div with a given ID.
+function changeVisibility (divID){
+
+	var element = document.getElementById(divID);
+
+	// iff element exitsts, toggle its class
+	// between hidden and unhidden
+
+	if(element){
+		element.className = (element.className == 'hidden')? 'unhidden':'hidden';
+	}// if
+}//changeVisibility
+
+// display lightbox with image in it
+function displayLightBox(imageFile, alt){
+
+	var image = new Image();
+	var bigImage = document.getElementById("bigImage");
+
+	image.src = "images/" + imageFile;
+	image.alt = alt;
+
+	image.onload = function(){
+		var width =  image.width;
+		document.getElementById("boundaryBigImage").style.width = width + "px";
+	}
+
+	bigImage.src = image.src;
+	bigImage.alt = image.alt;
+	changeVisibility('lightbox');
+	changeVisibility('boundaryBigImage');
+}
